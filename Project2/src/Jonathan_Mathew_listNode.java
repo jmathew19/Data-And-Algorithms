f8f7f3fc6bbc192bdb65814fe76fd6e48afe3acf
Jonathan Mathew <33427456+jmathew19@users.noreply.github.com>
2022-11-21 20:36:34 -0500

    Delete Project2 directory

--- Project2/src/Jonathan_Mathew_listNode.java
+++
@@ -1,9 +0,0 @@
-public class Jonathan_Mathew_listNode {
-
-    String data;
-    Jonathan_Mathew_listNode next;
-
-    public Jonathan_Mathew_listNode(String data){
-        this.data = data;
-    }
-}
